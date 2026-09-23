e13d55608e8c59c7616b9ab8d09280edd7abc788
kantasurashe750-oss <dnyaneshwarsurashe750@gmail.com>
2026-09-23 16:34:22 +0530

    Learn fom submittion

--- my-react-project/src/App.jsx
+++ my-react-project/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+
+// import { useState } from "react"
 import Header from "./Header"
 import Footer from "./footer"
 // import Student from "./Student"
@@ -32,27 +33,27 @@ import Footer from "./footer"
 // )
 // }
 
-function App(){
+// function App(){
 
-  const[ count , setCount] = useState(5)
+//   const[ count , setCount] = useState(5)
 
-function handleClick(){
+// function handleClick(){
 
-  //setmessage("Welcome to my Collge")
-    setCount(count + 1)
-}
+//   //setmessage("Welcome to my Collge")
+//     setCount(count + 1)
+// }
 
-return(
-  <div>
-    <button onClick={handleClick}>
-        click me
-    </button>
+// return(
+//   <div>
+//     <button onClick={handleClick}>
+//         click me
+//     </button>
 
-    <p>{message}</p>
-  </div>
-)
-}
-export default App 
+//     <p>{count}</p>
+//   </div>
+// )
+// }
+// export default App 
 
 // function Header (){
 
@@ -69,3 +70,174 @@ export default App
 // }
 
 
+//useState
+
+
+
+// function App(){
+
+//   const [count , setCount] = useState(5)
+
+//   function handleclicked(){
+
+//     setCount(count-1)
+    
+//   }
+
+//   function handleclicked1(){
+
+//     setCount(count+1)
+//   }
+
+//   return(
+
+//     <div>
+//       <button onClick={handleclicked}>-</button>
+//       <button onClick={handleclicked1}>+</button>
+
+//       <p>{count}</p>
+//     </div>
+    
+//   )
+
+// }
+// import { useState } from "react"
+
+//ternary operator
+// function App(){
+
+//   const isLoggedIn = false
+
+//   return(
+//    <div>
+//     {isLoggedIn ? <h2>Welcome User!</h2> : <h2>Please Login.</h2>}
+//    </div>
+//   )
+// }
+
+//logical AND &&& rendering 
+
+// function App(){
+
+//   const isAdmin = false 
+
+//   return(
+//     <div>
+//       {isAdmin && <p>Admin Panel</p>}
+//     </div>
+//   )
+// }
+
+// function App(){
+
+//   const isOnline = true
+
+// return(
+//   <div>
+//     {isOnline ? <h2>User is Online</h2> : <h2>user is offline</h2>}
+//   </div>
+// )
+// }
+
+//Lists And Keys
+// function App(){
+
+//   const fruits = ["Apple " , "Banana" , "Mango"]
+
+//   return(
+//     <div>
+//       {fruits.map((fruit) => (
+//         <h2 key={fruit}>{fruit}</h2>
+//       ))}
+//     </div>
+//   )
+// }
+
+// List and Key complete
+
+// function App(){
+
+//   const products = [
+//   { id: 1, name: "Laptop", price: 50000 },
+//   { id: 2, name: "Mouse", price: 800 },
+//   { id: 3, name: "Keyboard", price: 1500 }
+// ]
+
+// return(
+//   <div>
+//     {products.map((product)=>(
+//       <h2 key={product.id}>
+//       name: {product.name} <br />
+//       price : {product.price}
+//       </h2>
+//     ))}
+//   </div>
+  
+// )
+// }
+
+//Forms part 1 - reading
+
+// import { useState } from "react"
+
+// function App(){
+
+//   const [name, setName] = useState("")
+
+//   return(
+//     <div>
+//       <input type="text" 
+//       value={name}
+//       onChange={(event)=>{
+//         setName(event.target.value)
+//       }}
+//       />
+//       <button>submit</button>
+//       <p>{name}</p>
+//     </div>
+//   )
+// }
+
+//form part 2 - Handling Submit
+
+import { useState } from "react"
+
+function App(){
+
+  const[name, setName] = useState("")
+
+  function handleSubmit(event){
+
+    event.preventDefault()
+    console.log(name);
+    
+  }
+
+  return(
+    // <div>
+    //   <input type="text" 
+    //   value={name} 
+    //   onChange={(event) =>{
+    //     setName(event.target.value)
+    //   }} 
+    //   />
+    // <button onSubmit={handleSubmit}>Submit</button>
+    // <p>{name}</p> 
+    // </div>
+
+    <form onSubmit={handleSubmit}>
+
+      <input type="text" 
+      value={name} 
+      onChange={(event) =>{
+        setName(event.target.value)
+      }} />
+
+      <button type="submit">submit</button>
+
+      <p>{name}</p>
+    </form>
+    
+  )
+}
+export default App
